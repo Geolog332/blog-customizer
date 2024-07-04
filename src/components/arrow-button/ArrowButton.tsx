ae360ffc1c29374b000d97b67eedc62a2a86edea
Anton Mataev <Geolog332@yahoo.com>
2024-07-04 23:56:09 +0500

    FirstTry

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,34 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
-/** Функция для обработки открытия/закрытия формы */
+// Функция для обработки открытия/закрытия формы
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type ArrowButtonProps = {
+	// Флаг состояния формы
+	form?: boolean;
+	// Функция, вызываемая при открытии/закрытии формы
+	onClick?: OnClick;
+};
+
+// Компонент кнопки со стрелкой для открытия/закрытия формы
+
+export const ArrowButton = ({ form, onClick }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+		// Не забываем указаывать role и aria-label атрибуты для интерактивных элементов
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, { [styles.container_open]: form })}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, { [styles.arrow_open]: form })}
+			/>
 		</div>
 	);
 };
